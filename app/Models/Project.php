165447a8cc9b2cbe93967270d57c488e2b8627ef
Rocca-Francesco <roccafrance98@yahoo.it>
2023-04-19 17:42:22 +0200

    mutator image

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -25,4 +25,8 @@ class Project extends Model
 
         return $possible_slug;
     }
+
+    protected function getLinkAttribute($value) {
+        return $value ? asset('storage/' . $value) : 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg';
+    }
 }
