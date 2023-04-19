6b0d75a9c1adab26c176023c22e5d5eea5c2078d
Rocca-Francesco <roccafrance98@yahoo.it>
2023-04-19 15:50:50 +0200

    anteprima immagine

--- resources/views/admin/form.blade.php
+++ resources/views/admin/form.blade.php
@@ -1,23 +1,28 @@
 @extends('layouts.app')
 
+@section('cdns')
+{{-- bootstrap icons --}}
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+@endsection
+
 @section('content')
 
 <div class="container">
     @if($project->id)
+    <div class="d-flex justify-content-between align-items-center mt-4">
       <h2 class="fs-4 text-secondary">
-      Create new project
-      </h2>
-    @else
-      <div class="d-flex justify-content-between align-items-center mt-4">
-        <h2 class="fs-4 text-secondary">
         Edit Project {{$project->title}}
-        </h2>
-        <div>
-          <a href="{{route('admin.projects.index')}}" class="btn btn-primary">Torna alla lista</a>
-          <a href="{{route('admin.projects.show', $project)}}" class="btn btn-primary my-2"><i class="bi bi-eye"></i></a>
-          <button type="submit" class="btn btn-danger my-2" data-bs-toggle="modal" data-bs-target="#destroyModal-{{$project->id}}"><i class="bi bi-trash3"></i></button>
-        </div>
+      </h2>
+      <div>
+        <a href="{{route('admin.projects.index')}}" class="btn btn-primary">Torna alla lista</a>
+        <a href="{{route('admin.projects.show', $project)}}" class="btn btn-primary my-2"><i class="bi bi-eye"></i></a>
+        <button type="submit" class="btn btn-danger my-2" data-bs-toggle="modal" data-bs-target="#destroyModal-{{$project->id}}"><i class="bi bi-trash3"></i></button>
       </div>
+    </div>
+    @else
+    <h2 class="fs-4 text-secondary">
+    Create new project
+    </h2>
     @endif
 
   @if($project->id)
@@ -30,7 +35,7 @@
 
     <div class="mb-3">
       <label for="title" class="form-label">Project Title</label>
-      <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title', $post->title)}}">
+      <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title', $project->title)}}">
       @error('title')
       <div class="invalid-feedback">
         {{ $message }}
@@ -38,24 +43,27 @@
       @enderror
     </div>
 
-    <div class="row row-cols-2">
-      <div class="mb-3">
+    <div class="row">
+      <div class="col-6 mb-3">
         <label for="lenguages" class="form-label">Types of lenguages</label>
-        <input type="text" class="form-control @error('lenguages') is-invalid @enderror" id="lenguages" name="lenguages" value="{{old('lenguages', $post->lenguages)}}">
+        <input type="text" class="form-control @error('lenguages') is-invalid @enderror" id="lenguages" name="lenguages" value="{{old('lenguages', $project->lenguages)}}">
         @error('title')
         <div class="invalid-feedback">
           {{ $message }}
         </div>
         @enderror
       </div>
-      <div class="mb-3">
+      <div class="col-3 mb-3">
         <label for="link" class="form-label">Link to the project</label>
-        <input type="url" class="form-control @error('lenguages') is-invalid @enderror" id="link" name="link" value="{{old('link', $post->link)}}">
+        <input type="url" class="form-control @error('lenguages') is-invalid @enderror" id="link" name="link" value="{{old('link', $project->link)}}">
         @error('title')
         <div class="invalid-feedback">
           {{ $message }}
         </div>
         @enderror
+      </div>
+      <div class="col-2">
+        <img src="{{old('link', $project->link)}}" class="img-fluid" alt="">
       </div>
     </div>
     <input type="submit" class="btn btn-success" value="Save">
